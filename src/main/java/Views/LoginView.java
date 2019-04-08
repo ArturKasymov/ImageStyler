bf86d43007a877a97127884c917d479dbd03b329
Demian <dembanakh01@gmail.com>
2019-04-09 00:41:46 +0200

    added LoginView simple structure

--- src/main/java/Views/LoginView.java
+++ src/main/java/Views/LoginView.java
@@ -1,15 +1,31 @@
 package Views;
 
 import Presenters.Interfaces.LoginPresenter;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginView {
     private LoginPresenter presenter;
 
-    /*onCLick {
-        presenter.Login()
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Hyperlink registerLink;
+
+    @FXML
+    public void initialize() {
+        loginButton.setDisable(true);
+        // TODO: set-on eventListeners
     }
 
-
-
-    */
 }
