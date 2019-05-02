add58772435a1fde5f501d9ce33368216fa49c6e
Demian <dembanakh01@gmail.com>
2019-05-02 16:36:32 +0200

    server fixes

--- server/src/main/java/util/Constants.java
+++ server/src/main/java/util/Constants.java
@@ -3,9 +3,9 @@ package util;
 public class Constants {
     public static final int DEFAULT_SERVER_PORT=7777;
     public static final String DEFAULT_DB_IP="//localhost";
-    public static final String DEFAULT_DB_USERNAME="imagestyler";
+    public static final String DEFAULT_DB_USERNAME="postgres";
     public static final String DEFAULT_DB_PASSWORD="imagestyler";
-    public static final String DEFAULT_DATABASE_NAME="serverDatabase";
+    public static final String DEFAULT_DATABASE_NAME="postgres";
     public static final int DEFAULT_DB_PORT=5432;
 
     public static final String SERVER_ROOT_DIRECTORY=System.getProperty("user.home")+"/.ImageStyler";
--- server/src/main/java/util/SQLiteQueries.java
+++ server/src/main/java/util/SQLiteQueries.java
@@ -3,19 +3,19 @@ package util;
 public class SQLiteQueries {
     public static final String CHECK_USERS="create table IF NOT EXISTS users (\n" +
             "id_user serial primary key,\n" +
-            "user_name varchar(32) not null unique," +
-            "password_hash varchar(32) not null" +
+            "user_name varchar(32) not null unique,\n" +
+            "password_hash varchar(32) not null\n" +
             ");";
     public static final String CHECK_SESSIONS="create table IF NOT EXISTS sessions (\n" +
             "id_session serial primary key,\n" +
             "id_user integer references users,\n" +
-            "isActive boolean" +
+            "isActive boolean\n" +
             ");";
     public static final String CHECK_FOTOS=" CREATE TABLE IF NOT EXISTS user_images(\n" +
             "id_image serial PRIMARY KEY,\n" +
             "image_name VARCHAR(32) NOT NULL,\n" +
             "id_user INTEGER REFERENCES users,\n" +
-            "image_date TIMESTAMP DEFAULT NOW(),\n"+
+            "image_date TIMESTAMP DEFAULT NOW()\n"+
             ");";
 
 }
