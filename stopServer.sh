516caa47b1ac5a249070f47e570934e4a443ee62
Artur <44034418+ArturKasymov@users.noreply.github.com>
2019-05-06 11:22:31 +0200

    Update stopServer.sh

--- stopServer.sh
+++ stopServer.sh
@@ -1,2 +1,1 @@
-sudo su -
-kill -9 (lsof -n -i | grep "\*\:7777" | sed -r 's/java[ ]*(\b[0-9]+\b).*$/\1/')
+(sudo su - ; lsof -n -i | grep "\*\:7777" | sed -r 's/java[ ]*(\b[0-9]+\b).*$/\1/' | xargs -I {} kill -9 {} )
